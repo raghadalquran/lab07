826d5c70ad43cba8c89e129c0cd08042c45ac6b0
raghadalquran <raghadalquran1@gmail.com>
2020-04-06 20:05:23 +0300

    commit

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 'use strict';
-// Load Environment Variables from the .env file
+
+//  In this step i load the Environment Variables from the .env file
 require('dotenv').config();
-// Application Dependencies
+
+// Dependencies
 const express = require('express');
 const cors = require('cors');
-
-// Application Setup
-const PORT = process.env.PORT || 4000;
-const app = express();
 const superagent = require('superagent');
+//  Setup My Application
+const PORT = process.env.PORT;
+const app = express();
 app.use(cors());
-// API Routes
+
 app.get('/', (request, response) => {
-  response.status(200).send('It Works!');
+  response.send('Home Page!');
 });
-// app.get('/bad', (request, response) => {
-//   throw new Error('oh nooooo!');
-// });
+
+// Route Definitions
 app.get('/location', locationHandler);
+app.get('/weather', weatherHandler);
+app.get('/trails',trailsHandler);
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+// Route Handlers
 function locationHandler(request, response) {
   const city = request.query.city;
+  let key = process.env.GEOCODE_API_KEY;
   superagent(
-    `https://eu1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json`
+    `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`
   )
     .then((res) => {
       const geoData = res.body;
@@ -31,40 +38,68 @@ function locationHandler(request, response) {
     .catch((err) => errorHandler(err, request, response));
 }
 
-app.get('/weather', weatherHandler());
-function weatherHandler(request, response) {
-  superagent(
-    `https://api.weatherbit.io/v2.0/forecast/daily?city=${request.query.search_query}&key=${process.env.WEATHER_API_KEY}`
-  )
-    .then((weatherRes) => {
-      console.log(weatherRes);
-      const weatherSummaries = weatherRes.body.data.map((val) => {
-        return new Weather(val);
-      });
-      response.status(200).json(weatherSummaries);
-    })
-    .catch((err) => errorHandler(err, request, response));
-}
-
-app.use('*', notFoundHandler);
-//The constructor function of the Location
 function Location(city, geoData) {
   this.search_query = city;
   this.formatted_query = geoData[0].display_name;
   this.latitude = geoData[0].lat;
   this.longitude = geoData[0].lon;
 }
-//The constructor function of the Weather
-function Weather(city,val){
-  this.forecast=val.weather.description;
-  this.time= new Date (val.valid_date).toString().slice(0,15);
+
+function weatherHandler(request, response) {
+  let key2 = process.env.WEATHER_API_KEY;
+  superagent(
+    `https://api.weatherbit.io/v2.0/forecast/daily?city=${request.query.search_query}&key=${key2}`
+  )
+    .then((weatherRes) => {
+      console.log(weatherRes);
+      const weatherSummaries = weatherRes.body.data.map((day) => {
+        return new Weather(day);
+      });
+      response.status(200).json(weatherSummaries);
+    })
+    .catch((err) => errorHandler(err, request, response));
 }
-// HELPER FUNCTIONS
+
+function Weather(day) {
+  this.forecast = day.weather.description;
+  this.time = new Date(day.valid_date).toString().slice(0, 15);
+}
+
+function trailsHandler(request,response){
+  let lat = request.query.latitude;
+  let lon = request.query.longitude;
+  let key3 = process.env.TRAIL_API_KEY;
+
+  superagent(
+    `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${key3}`
+  )
+    .then((trailsRes)=>{
+      const trailsSummaries = trailsRes.body.trails.map((val)=>{
+        return new Trails (val);
+      });
+      response.status(200).json(trailsSummaries);})
+    .catch((err)=> errorHandler(err, request, response));
+}
+function Trails (val){
+  this.name = val[0].name;
+  this.location = val[0].location;
+  this.length = val[0].length;
+  this.stars = val[0].stars;
+  this.star_votes = val[0].starVotes;
+  this.summary = val[0].summary;
+  this.trail_url = val[0].url;
+  this.conditions = val[0].conditionDetails;
+  this.condition_date = val[0].conditionDate;
+  this.condition_time = val[0].conditionDate;
+}
+
 function notFoundHandler(request, response) {
-  response.status(404).send('NOT FOUND!!');
+  response.status(404).send('404 NOT FOUND');
 }
+
 function errorHandler(error, request, response) {
   response.status(500).send(error);
 }
+
 // Make sure the server is listening for requests
-app.listen(PORT, () => console.log(`the server is up and running on ${PORT}`));
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
